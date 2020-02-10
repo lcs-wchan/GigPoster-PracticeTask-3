7bb875f0e3c17917d79ef2f955bc68137831dab2
WilliamChan <wchan@lcs.on.ca>
2020-02-10 15:06:30 -0500

    finished diagonal lines

--- Gig Poster - Practice - 3.playground/Pages/Modest Mouse.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 3.playground/Pages/Modest Mouse.xcplaygroundpage/Contents.swift
@@ -32,6 +32,38 @@ let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 
 // Begin your solution here...
 
+// draw background
+canvas.fillColor = darkGrey
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+
+// draw grid
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+for y in stride (from: 0, through: 600, by: 50){
+    for x in stride (from: 0, through: 400, by: 50){
+        canvas.drawRectangle(at: Point(x: x, y: y), width: 50, height: 50)
+    }
+}
+
+// draw diagonal line
+canvas.rotate(by: -45)
+for x in stride(from: -300, to: 400, by: 40){
+    if x < -80{
+        canvas.lineColor = lightGrey
+    }
+    if -80 < x && x < 50 {
+        canvas.lineColor = deepYellow
+    }
+    if x > 50{
+        canvas.lineColor = black
+    }
+    
+    canvas.drawLine(from: Point(x: x, y: 0), to: Point(x: x, y: 700), lineWidth: 20)
+}
+    
+// draw another background
+
 
 /*:
  ## Use Source Control
